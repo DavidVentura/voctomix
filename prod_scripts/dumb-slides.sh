95e22b2c9731aa80f3dab7412553be7c34839f18
David <davidventura27@gmail.com>
2017-04-24 23:08:54 -0300

    update dumb slides to gstreamer

--- prod_scripts/dumb-slides.sh
+++ prod_scripts/dumb-slides.sh
@@ -6,17 +6,12 @@ if [ -f $confdir/config.sh ]; then
 fi
 
 while [ true ]; do
-	nc -l -p 5001 | ffmpeg -loglevel error -y -nostdin -xerror -hide_banner \
-		-use_wallclock_as_timestamps 1 \
-		-f h264 -probesize 32 -analyzeduration 1000 -i - \
-		-filter_complex "[0:v] scale=$WIDTH:$HEIGHT,fps=$FRAMERATE [v]" \
-		-map "[v]"\
-		-c:v rawvideo \
-		-pix_fmt yuv420p \
-		-f matroska \
-		tcp://localhost:10000
+	gst-launch-1.0 -qe tcpclientsrc host=192.168.2.208 port=5002 !\
+		matroskademux ! h264parse ! avdec_h264 !\
+		videorate ! videoscale add-borders=false ! videoconvert !\
+		video/x-raw,format=I420,width=1280,height=720,framerate=25/1,pixel-aspect-ratio=1/1 !\
+		matroskamux ! tcpclientsink host=localhost port=10000
 
-	echo $?
 	echo "the stream thingy died"
 	sleep 1
 done
