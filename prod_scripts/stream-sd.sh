cd1fc610fbfbc52ff33aedb32397c30bf4bed2b7
David <davidventura27@gmail.com>
2017-05-11 09:39:01 -0300

    change crf to qmin, add maxrate

--- prod_scripts/stream-sd.sh
+++ prod_scripts/stream-sd.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+MAXRATE=1100
+MAXRATE=600
+MAXRATE=1000
 ffmpeg -y -nostdin -hide_banner -loglevel warning -stats \
 	-i tcp://localhost:15000 \
 	-threads:0 0 \
 	-aspect 16:9 \
 	-c:v libx264 \
-	-maxrate:v:0 1100k -bufsize:v:0 8192k -crf:0 21 \
+	-maxrate:v:0 "${MAXRATE}k" -bufsize:v:0 8192k -qmin 21 \
 	-pix_fmt:0 yuv420p -profile:v:0 high -g:v:0 50 \
 	-preset:v:0 veryfast \
 	\
@@ -12,3 +15,5 @@ ffmpeg -y -nostdin -hide_banner -loglevel warning -stats \
 	-ac:a:2 2 \
 	\
 	-y -f flv rtmp://192.168.2.120:1935/rpi2/movie
+	#-maxrate:v:0 1100k -bufsize:v:0 8192k -crf:0 21 \
+	# -maxrate:v:0 "${MAXRATE}k" -bufsize:v:0 8192k -crf:0 21 \
