83adfe0fdc0386befbb1eb116042cd199f3cdc5d
David <davidventura27@gmail.com>
2017-07-08 20:37:00 -0300

    increase bitrate, use a slower video preset

--- prod_scripts/stream-sd.sh
+++ prod_scripts/stream-sd.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 MAXRATE=1100
 MAXRATE=600
+MAXRATE=900
 MAXRATE=1000
-MAXRATE=950
 ffmpeg -y -nostdin -hide_banner -loglevel warning -stats \
 	-i tcp://localhost:15000 \
 	-threads:0 3 \
@@ -10,9 +10,9 @@ ffmpeg -y -nostdin -hide_banner -loglevel warning -stats \
 	-c:v libx264 \
 	-maxrate "${MAXRATE}k" -bufsize:v:0 2048k -qmin 21 \
 	-pix_fmt:0 yuv420p -profile:v:0 high -g:v:0 50 \
-	-preset:v:0 veryfast \
+	-preset:v:0 fast \
 	\
-	-ac 1 -c:a libfdk_aac -b:a 96k -ar 44100 \
+	-ac 1 -c:a libfdk_aac -b:a 64k -ar 44100 \
 	-ac:a:2 2 \
 	\
 	-y -f flv rtmp://192.168.2.120:1935/rpi2/movie
