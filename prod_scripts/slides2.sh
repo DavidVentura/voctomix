e23a1fabc63190260bbc6260a30a45d0edb9124e
David <davidventura27@gmail.com>
2017-07-09 22:19:11 -0300

    add audio to slides

--- prod_scripts/slides2.sh
+++ prod_scripts/slides2.sh
@@ -5,9 +5,16 @@ if [ -f $confdir/config.sh ]; then
 	. $confdir/config.sh
 fi
 
-gst-launch-1.0 -qe udpsrc address=239.255.42.42 port=5004 do-timestamp=true !\
-	queue ! tsdemux ! queue ! h264parse ! avdec_h264 !\
-	videorate ! videoscale ! videoconvert !\
+AUDIOSRC='audiotestsrc freq=400 volume=0'
+AUDIOSRC='d. ! queue ! mpegaudioparse ! avdec_mp3 ! queue' 
+gst-launch-1.0 -qe \
+    udpsrc address=239.255.42.42 port=5004 do-timestamp=true ! queue !\
+    tsdemux name=d !\
+    queue ! h264parse ! avdec_h264 !\
+	videorate ! videoscale add-borders=false ! videoconvert !\
 	video/x-raw,format=I420,width=$WIDTH,height=$HEIGHT,framerate=$FRAMERATE/1,pixel-aspect-ratio=1/1 !\
-	queue ! matroskamux ! queue ! tcpclientsink blocksize=16384 host=localhost port=10000
-echo "the stream thingy died"
+	m. $AUDIOSRC !\
+    audioconvert ! audioresample ! audiorate!\
+    audio/x-raw,format=S16LE,channels=2,layout=interleaved,rate=$AUDIORATE !\
+    matroskamux name=m ! queue ! tcpclientsink blocksize=16384 host=localhost port=10000
+    #queue ! audioconvert ! audioresample ! audiorate ! \
