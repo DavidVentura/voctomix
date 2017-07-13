d04e664ff289415d4b08d8484433cb00cdb71ca9
David <davidventura27@gmail.com>
2017-07-13 20:28:05 -0300

    sanitize input

--- prod_scripts/generator.sh
+++ prod_scripts/generator.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 source ./default-config.sh
 #ffmpeg -i ../blanker.h264 -c:v rawvideo -pix_fmt:v yuv420p -c:v rawvideo -pix_fmt yuv420p -r 25 -f rawvideo blanker.raw 
-ffmpeg -i background.png  -c:v rawvideo -pix_fmt:v yuv420p -c:v rawvideo -pix_fmt yuv420p -s "${WIDTH}x${HEIGHT}" -frames 1 -f rawvideo background.raw 
+if [ $# -ne 1 ]; then
+    echo "usage $0 filename"
+    exit 1
+fi
+if [ ! -f "$1" ]; then
+    echo "$1 not a file"
+    exit 1
+fi
+ffmpeg -i "$1" -c:v rawvideo -pix_fmt:v yuv420p -c:v rawvideo -pix_fmt yuv420p -s "${WIDTH}x${HEIGHT}" -frames 1 -f rawvideo background.raw 
