aaede734479d604355f8cc9176ac13c18c89e100
David <davidventura27@gmail.com>
2017-07-13 22:20:09 -0300

    static text for the bg

---
+++ prod_scripts/bg-with-text.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -eu
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 class name"
+    exit 1
+fi
+
+FONT1="Museo Sans, 11"
+FONT2="Museo Sans Cyrl 100 11"
+BLACK="4278190080"
+
+TITLE="Estás viendo:"
+CLASS="$1"
+
+length=${#CLASS}
+MAXLENGTH=48
+if [ $length -gt $MAXLENGTH ]; then
+    echo "Max length for classname is $MAXLENGTH, your input has $length chars"
+    exit 1
+fi
+
+gst-launch-1.0 -q filesrc location='/home/nginx/str.png' ! pngdec ! videoconvert ! video/x-raw,format=I420 ! imagefreeze ! video/x-raw,framerate=25/1 !\
+    textoverlay text="$CLASS"\
+    valignment=absolute halignment=4 y-absolute=0.91 x-absolute=0 deltax=15 font-desc="$FONT1" draw-shadow=false draw-outline=false color="$BLACK" !\
+    textoverlay text="$TITLE"\
+    valignment=absolute halignment=4 y-absolute=0.85 x-absolute=0 deltax=15 font-desc="$FONT2" draw-shadow=false draw-outline=false color="$BLACK" !\
+    queue ! matroskamux ! tcpclientsink blocksize=16384 host=localhost port=16000
