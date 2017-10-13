a8364d33e31fea990e13cd8353ee3d9d8de1b023
David <davidventura27@gmail.com>
2017-10-13 09:43:32 -0300

    lower bandwidth even more

--- prod_scripts/stream-sd.sh
+++ prod_scripts/stream-sd.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 MAXRATE=1100
 MAXRATE=600
-MAXRATE=900
 MAXRATE=1000
+MAXRATE=750
+
+# -qmax 30 -qmin 24 \
 ffmpeg -y -nostdin -hide_banner -loglevel warning -stats \
 	-i tcp://localhost:15000 \
 	-threads:0 3 \
 	-aspect 16:9 \
 	-c:v libx264 \
-	-maxrate "${MAXRATE}k" -bufsize:v:0 2048k -qmin 21 \
-	-pix_fmt:0 yuv420p -profile:v:0 high -g:v:0 50 \
+    -maxrate "${MAXRATE}k" \
+    -bufsize:v:0 2048k \
+	-pix_fmt:0 yuv420p -profile:v:0 high -g:v:0 75 \
 	-preset:v:0 fast \
 	\
 	-ac 1 -c:a libfdk_aac -b:a 64k -ar 44100 \
-	-ac:a:2 2 \
 	\
 	-y -f flv rtmp://192.168.2.120:1935/rpi2/movie
+	# -maxrate "${MAXRATE}k" -bufsize:v:0 2048k -qmin 21 \
+
 	#-maxrate:v:0 1100k -bufsize:v:0 8192k -crf:0 21 \
 	# -maxrate:v:0 "${MAXRATE}k" -bufsize:v:0 8192k -crf:0 21 \
