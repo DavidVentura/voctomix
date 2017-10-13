a0be2fbf2fa74f53eec7e3e5d01c252313570f73
David <davidventura27@gmail.com>
2017-10-13 09:43:11 -0300

    adjust overscan and videoflip to match new projector

--- prod_scripts/slides2.sh
+++ prod_scripts/slides2.sh
@@ -7,11 +7,12 @@ fi
 
 AUDIOSRC='audiotestsrc freq=400 volume=0'
 AUDIOSRC='d. ! queue ! mpegaudioparse ! avdec_mp3 ! queue' 
-WITH_AUDIO=""
 gst-launch-1.0 -q \
     udpsrc address=239.255.42.42 port=5004 do-timestamp=true ! queue !\
     tsdemux name=d !\
     queue ! h264parse ! avdec_h264 !\
+    videocrop top=64 left=64 right=64 bottom=64 !\
+    videoflip method=rotate-180 !\
 	videorate ! videoscale add-borders=false ! videoconvert !\
 	video/x-raw,format=I420,width=$WIDTH,height=$HEIGHT,framerate=$FRAMERATE/1,pixel-aspect-ratio=1/1 !\
 	m. $AUDIOSRC !\
