5e93f4b5326387cba43b228bf18ccc04389bf7c6
David <davidventura27@gmail.com>
2017-10-30 00:27:31 -0300

    write to fifo directly with message passed

--- prod_scripts/timer.py
+++ prod_scripts/timer.py
@@ -1,47 +1,52 @@
 #!/usr/bin/env python3
 import datetime
 import sys
+import time
 
 def minutes(t):
-	hour = int(t.split(":")[0])
-	minute = int(t.split(":")[1])
+    hour = int(t.split(":")[0])
+    minute = int(t.split(":")[1])
 
-	now     = datetime.datetime.now()
-	target  = now.replace(hour=hour, minute=minute, second=0, microsecond=0)
-	
-	delta = target-now
-	if delta.days < 0:
-		return 1 #1 minute if we have gone over the top
+    now     = datetime.datetime.now()
+    target  = now.replace(hour=hour, minute=minute, second=0, microsecond=0)
+    
+    delta = target-now
+    if delta.days < 0:
+        return 1 #1 minute if we have gone over the top
 
-	minutes_left = int(delta.seconds/60)
-	minutes_left = max(minutes_left, 1)
-	return minutes_left
+    minutes_left = int(delta.seconds/60)
+    minutes_left = max(minutes_left, 1)
+    return minutes_left
 
 if __name__ == "__main__":
-	if len(sys.argv) != 3:
-		print("usage: %s HH:MM Text" % sys.argv[0])
-		print("Text must contain exactly one '%s'")
-		sys.exit(1)
+    if len(sys.argv) != 3:
+        print("usage: %s HH:MM Text" % sys.argv[0])
+        print("Text must contain exactly one '%s'")
+        sys.exit(1)
 
-	t = sys.argv[1]
-	if t.count(":") != 1:
-		print("Time format: HH:MM")
-		sys.exit(1)
+    t = sys.argv[1]
+    if t.count(":") != 1:
+        print("Time format: HH:MM")
+        sys.exit(1)
 
-	text = sys.argv[2]
-	if text.count("%s") != 1:
-		print("Text must contain exactly one '%s'")
-		sys.exit(1)
+    text = sys.argv[2]
+    if text.count("%s") != 1:
+        print("Text must contain exactly one '%s'")
+        sys.exit(1)
 
-	try:
-		m = max(minutes(t), 1)
-	except Exception as e:
-		print(e)
-		sys.exit(1)
+    try:
+        m = max(minutes(t), 1)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
 
 
-	text = (text % m)
-	if m == 1:
-		text = text.replace("minutos", "minuto")
-
-	print("text=%s" % text)
+    while True:
+        fifo = open("/tmp/blankerfifo", "w")
+        outtext = (text % m)
+        if m == 1:
+            outtext = text.replace("minutos", "minuto")
+            break
+        fifo.write("text=%s\n" % outtext)
+        fifo.close()
+        time.sleep(30)
