302c85f4e26d8c72f9c73f94fb6d500c08cb4e7b
David <davidventura27@gmail.com>
2017-11-10 15:39:57 -0300

    adjust crop for overscanning

--- prod_scripts/slides2.sh
+++ prod_scripts/slides2.sh
@@ -7,11 +7,12 @@ fi
 
 AUDIOSRC='audiotestsrc freq=400 volume=0'
 AUDIOSRC='d. ! queue ! mpegaudioparse ! avdec_mp3 ! queue' 
+# videocrop top=48 left=64 right=64 bottom=48 !\
 exec gst-launch-1.0 -q \
     udpsrc address=239.255.42.42 port=5004 do-timestamp=true ! queue !\
     tsdemux name=d !\
     queue ! h264parse ! avdec_h264 !\
-    videocrop top=48 left=64 right=64 bottom=48 !\
+    videocrop top=72 left=96 right=96 bottom=72 !\
     videoflip method=rotate-180 !\
 	videorate ! videoscale add-borders=false ! videoconvert !\
 	video/x-raw,format=I420,width=$WIDTH,height=$HEIGHT,framerate=$FRAMERATE/1,pixel-aspect-ratio=1/1 !\
